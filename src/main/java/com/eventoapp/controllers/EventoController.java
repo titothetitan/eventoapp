730dfd3e7bd761c178c2eb1663c437e04af90dda
titothetitan <tito.the.titan@gmail.com>
2020-12-05 14:55:31 -0300

    Casa Tarde 1

--- src/main/java/com/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,5 +1,12 @@
 package com.eventoapp.controllers;
 
-public class EventoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class EventoController {
+	@RequestMapping("evento/cadastrarEvento")
+	public String form() {
+		return "formEvento";
+	}
 }
